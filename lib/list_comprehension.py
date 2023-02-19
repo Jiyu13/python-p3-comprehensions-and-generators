407c2fe33044c4d5debfda4133d6e0e3fcc92a6c
Ziru Zou <jiyucoding@yahoo.com>
2023-02-18 20:47:54 -0500

    complete lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python3
 
-def return_evens(num_list):
-    pass
+#  create lists using list comprehensions
 
+
+# returns a list of all of the even elements of a sequence of integers
+def return_evens(num_list):
+    return [num for num in num_list if not num % 2 ]
+
+# returns a list of sentence strings with exclamation marks at the end
 def make_exclamation(sentence_list):
-    pass
+    return [f"{string}!" for string in sentence_list]
